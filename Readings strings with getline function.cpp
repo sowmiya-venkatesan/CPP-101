ad088490d6bc243986ab75ee771ecb50170d4aab
Sowmiya V <74345706+sowmiya-venkatesan@users.noreply.github.com>
2021-05-02 19:26:10 +0530

    Create Readings strings with getline function.cpp

---
+++ Readings strings with getline function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int size=20;
+    char city[20];
+    cout<<"Enter city name :\n";
+    cin>>city;
+    cout<<"city name:"<<city<<"\n\n";
+    cout<<"Enter city name again : \n";
+    cin.getline(city,size);
+    cout<<"City name now:"<<city<<"\n\n";
+    cout<<"Enter another city name again: \n";
+    cin.getline(city,size);
+    cout<<"New city name:"<<city<<"\n\n";
+    return 0;
+}
